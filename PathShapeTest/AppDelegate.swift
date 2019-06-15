7d77542942894eab3536767ca2bf59a39b321096
Daniel Tull <dt@danieltull.co.uk>
2019-06-15 11:04:20 -0700

    Add RelativePath shape

--- PathShapeTest/AppDelegate.swift
+++ PathShapeTest/AppDelegate.swift
@@ -16,17 +16,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-
-        window.contentView = NSHostingView(rootView: ContentView())
-
-        window.makeKeyAndOrderFront(nil)
+//        // Insert code here to initialize your application
+//        window = NSWindow(
+//            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+//            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+//            backing: .buffered, defer: false)
+//        window.center()
+//        window.setFrameAutosaveName("Main Window")
+//
+//        window.contentView = NSHostingView(rootView: ContentView())
+//
+//        window.makeKeyAndOrderFront(nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
--- PathShapeTest/ContentView.swift
+++ PathShapeTest/ContentView.swift
@@ -1,25 +1,65 @@
-//
-//  ContentView.swift
-//  PathShapeTest
-//
-//  Created by Daniel Tull on 15/06/2019.
-//  Copyright © 2019 Daniel Tull. All rights reserved.
-//
 
 import SwiftUI
 
-struct ContentView : View {
-    var body: some View {
-        Text("Hello World")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+struct RelativePath: Shape {
+
+    private let relative: Path
+    public init(_ callback: (inout Path) -> ()) {
+        var path = Path ()
+        callback(&path)
+        relative = path
+    }
+
+    func path(in rect: CGRect) -> Path {
+
+        func convert(_ point: CGPoint) -> CGPoint {
+            CGPoint(x: rect.origin.x + point.x * rect.size.width,
+                    y: rect.origin.y + point.y * rect.size.height)
+        }
+
+        return Path { absolute in
+
+            relative.forEach { element in
+
+                switch element {
+                case let .move(to):
+                    absolute.move(to: convert(to))
+
+                case let .line(to):
+                    absolute.addLine(to: convert(to))
+
+                case let .quadCurve(to, control):
+                    absolute.addQuadCurve(to: convert(to),
+                                          control: convert(control))
+
+                case let .curve(to, control1, control2):
+                    absolute.addCurve(to: convert(to),
+                                      control1: convert(control1),
+                                      control2: convert(control2))
+
+                case .closeSubpath:
+                    absolute.closeSubpath()
+
+                @unknown default:
+                    break
+                }
+            }
+
+        }
     }
 }
-
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RelativePath { path in
+            path.move(to: .init(x: 0, y: 0))
+            path.addLine(to: .init(x: 1, y: 1))
+            path.addLine(to: .init(x: 1, y: 0))
+            path.closeSubpath()
+        }
+        .fill(Color.orange)
+        .frame(width: 400, height: 200, alignment: .bottom)
     }
 }
 #endif
