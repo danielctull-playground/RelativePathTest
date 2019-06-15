3b312c206d4b790b1ea15a982c8a2f12be5e5abd
Daniel Tull <dt@danieltull.co.uk>
2019-06-15 11:25:07 -0700

    Rename project

---
+++ RelativePathTest/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  RelativePathTest
+//
+//  Created by Daniel Tull on 15/06/2019.
+//  Copyright © 2019 Daniel Tull. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow!
+}
